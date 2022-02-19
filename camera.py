142242fabbc55778ea050d0330592a65a6bf8b93
ktiscoding <100025035+ktiscoding@users.noreply.github.com>
2022-02-19 11:26:33 +0000

    Create camera.py

---
+++ camera.py
@@ -0,0 +1,22 @@
+import cv2
+
+cap = ""
+
+while cap == "":
+    cap = cv2.VideoCapture(0)
+    if not (cap.isOpened()):
+        print('Could not open video device')
+        cap = ""
+
+print("Show letter to camera")
+ret, frame = cap.read()
+cv2.imshow('preview',frame)
+
+cv2.waitKey(0)
+    
+
+cv2.imwrite("pic.png", frame)
+
+cap.release()
+
+cv2.destroyAllWindows()
