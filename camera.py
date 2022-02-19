0e0d6df0385e78f6801ab931031d0529046f14c2
algebraic-monkey <85030307+algebraic-monkey@users.noreply.github.com>
2022-02-19 11:33:07 +0000

    Update camera.py

--- camera.py
+++ camera.py
@@ -2,17 +2,23 @@ import cv2
 
 cap = ""
 
+#what happens below
+
 while cap == "":
     cap = cv2.VideoCapture(0)
     if not (cap.isOpened()):
         print('Could not open video device')
         cap = ""
+        
+#what happens below
 
 print("Show letter to camera")
 ret, frame = cap.read()
 cv2.imshow('preview',frame)
 
 cv2.waitKey(0)
+
+#what happens below
     
 
 cv2.imwrite("pic.png", frame)
