b4c33f3b347a40d03628fa81577cf83f07676038
ktiscoding <100025035+ktiscoding@users.noreply.github.com>
2022-02-20 08:22:43 +0000

    Update main.py

--- main.py
+++ main.py
@@ -58,38 +58,39 @@ class CodesecApp(App):
         cap.release()
 
         cv2.destroyAllWindows()
+        
+    def machineLearning(self):
+        # Load the model
+        model = load_model('keras_model.h5', compile=False)
+
+        # Create the array of the right shape to feed into the keras model
+        # The 'length' or number of images you can put into the array is
+        # determined by the first position in the shape tuple, in this case 1.
+        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+        image = Image.open('letter.png')
+        #resize the image to a 224x224 with the same strategy as in TM2:
+        #resizing the image to be at least 224x224 and then cropping from the center
+        size = (224, 224)
+        image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+        #turn the image into a numpy array
+        image_array = np.asarray(image)
+        # Normalize the image
+        normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+        # Load the image into the array
+        data[0] = normalized_image_array
+
+        letter=""
+        # run the inference
+        prediction = model.predict(data)
+        print(prediction)
+        if prediction[0][0]>prediction[0][1]:
+            letter="A"
+        else:
+            letter="not A"
+        print(letter)
 
 
 # This calls the App creating function
 if name == 'main':
     CodesecApp().run()
-
-# Load the model
-model = load_model('keras_model.h5', compile=False)
-
-# Create the array of the right shape to feed into the keras model
-# The 'length' or number of images you can put into the array is
-# determined by the first position in the shape tuple, in this case 1.
-data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-image = Image.open('letter.png')
-#resize the image to a 224x224 with the same strategy as in TM2:
-#resizing the image to be at least 224x224 and then cropping from the center
-size = (224, 224)
-image = ImageOps.fit(image, size, Image.ANTIALIAS)
-
-#turn the image into a numpy array
-image_array = np.asarray(image)
-# Normalize the image
-normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-# Load the image into the array
-data[0] = normalized_image_array
-
-letter=""
-# run the inference
-prediction = model.predict(data)
-print(prediction)
-if prediction[0][0]>prediction[0][1]:
-    letter="A"
-else:
-    letter="not A"
-print(letter)
